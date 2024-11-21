503f105be93410caeb03a610b12c4924b6faa54c
romeoscript <romeobourne211@gmail.com>
2024-11-21 03:25:21 +0100

    virtual card fix

---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import cardicon from "../assets/home/cardicon.svg";
+
+const CreditCard = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleToggle = () => {
+    setIsFlipped((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className="  bg-gray-100">
+        <div
+          className={`relative w-80 h-48 transform-style-preserve transition-transform duration-700 ${
+            isFlipped ? "rotate-y-180" : ""
+          }`}
+        >
+          {/* Front of the Card */}
+          <div
+            className={`absolute w-full h-full bg-custom-gradient from-blue-500 to-purple-600 text-white rounded-lg flex flex-col justify-between p-4 backface-hidden`}
+          >
+            <img src={cardicon} className="absolute h-full w-full top-0" />
+            <div className="flex justify-between">
+              <span className="text-sm font-bold">CREDIT CARD</span>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg"
+                alt="Mastercard Logo"
+                className="w-12"
+              />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-lg tracking-wider font-mono">
+                **** **** **** 3456
+              </p>
+              <div className="flex justify-between mt-2">
+                <div>
+                  <p className="text-xs uppercase">Cardholder</p>
+                  <p className="text-sm font-bold">John Doe</p>
+                </div>
+                <div>
+                  <p className="text-xs uppercase">Expiry</p>
+                  <p className="text-sm font-bold">08/24</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Back of the Card */}
+          <div
+            className={`absolute w-full h-full bg-custom-gradient text-white rounded-lg flex flex-col justify-center items-center p-4 transform rotate-y-180 backface-hidden`}
+          >
+            <img src={cardicon} className="absolute h-full w-full top-0" />
+            <p className="text-lg tracking-wider font-mono">
+              1234 5678 9012 3456
+            </p>
+            <div className="flex justify-between w-full mt-4">
+              <div>
+                <p className="text-xs uppercase text-gray-500">Expiry</p>
+                <p className="text-sm font-bold">08/24</p>
+              </div>
+              <div>
+                <p className="text-xs uppercase text-gray-500">CVV</p>
+                <p className="text-sm font-bold">123</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-start">
+        <button
+          onClick={handleToggle}
+          className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+        >
+          {isFlipped ? "Hide Details" : "Reveal Details"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CreditCard;
--- src/pages/Dashb/VirtualCard.jsx
+++ src/pages/Dashb/VirtualCard.jsx
@@ -1,75 +1,51 @@
-
 import React, { useState } from "react";
-import cardicon from "../../assets/home/cardicon.svg";
+import { CiCreditCard1 } from "react-icons/ci";
+import { MdBlock } from "react-icons/md";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import CreditCard from "../../components/CreditCard";
 
 const VirtualCard = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleToggle = () => {
-    setIsFlipped((prev) => !prev);
-  };
-
+const cardActions = [
+  {
+    name: "Change Card Pin",
+    icon: <CiCreditCard1 />,
+    text: "Easily change your virtual card pin.",
+  },
+  {
+    name: "Block Card",
+    icon: <MdBlock />,
+    text: "Temporarily disable your virtual card.",
+  },
+  {
+    name: "Cancel Card",
+    icon: <RiDeleteBin2Line />,
+    text: "Permanetly disable and delete your virtual card.",
+  },
+]
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div
-        className={`relative w-80 h-48 transform-style-preserve transition-transform duration-700 ${
-          isFlipped ? "rotate-y-180" : ""
-        }`}
-      >
-        {/* Front of the Card */}
-        <div
-          className={`absolute w-full h-full bg-custom-gradient from-blue-500 to-purple-600 text-white rounded-lg flex flex-col justify-between p-4 backface-hidden`}
-        >
-
-          <img src={cardicon} className="absolute h-full w-full top-0"/>
-          <div className="flex justify-between">
-            <span className="text-sm font-bold">CREDIT CARD</span>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg"
-              alt="Mastercard Logo"
-              className="w-12"
-            />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-lg tracking-wider font-mono">**** **** **** 3456</p>
-            <div className="flex justify-between mt-2">
-              <div>
-                <p className="text-xs uppercase">Cardholder</p>
-                <p className="text-sm font-bold">John Doe</p>
-              </div>
-              <div>
-                <p className="text-xs uppercase">Expiry</p>
-                <p className="text-sm font-bold">08/24</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Back of the Card */}
-        <div
-          className={`absolute w-full h-full bg-custom-gradient text-white rounded-lg flex flex-col justify-center items-center p-4 transform rotate-y-180 backface-hidden`}
-        >
-           <img src={cardicon} className="absolute h-full w-full top-0"/>
-          <p className="text-lg tracking-wider font-mono">1234 5678 9012 3456</p>
-          <div className="flex justify-between w-full mt-4">
-            <div>
-              <p className="text-xs uppercase text-gray-500">Expiry</p>
-              <p className="text-sm font-bold">08/24</p>
-            </div>
-            <div>
-              <p className="text-xs uppercase text-gray-500">CVV</p>
-              <p className="text-sm font-bold">123</p>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div className="text-left mb-[2rem] ">
+        <h2 className="font-bold text-2xl mb-[1rem]">Manage Card</h2>
+        <p className="text-gray-400">
+          Instantly create and manage virtual cards for secure online payments.
+          Enjoy the convenience and safety of using virtual cards for all your
+          online transactions, with real-time tracking and easy management
+          through your ConvertNG account.
+        </p>
       </div>
+      <CreditCard />
 
-      <button
-        onClick={handleToggle}
-        className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
-      >
-        {isFlipped ? "Hide Details" : "Reveal Details"}
-      </button>
+      <div className="my-[2rem]">
+        {cardActions.map((action, index) => (
+          <div key={index} className="flex items-center gap-8 bg-white p-[1rem] my-[1rem] rounded-md">
+            <div className="w-10 h-10 rounded-md bg-gray-200 flex items-center justify-center text-2xl">{action.icon}</div>
+            <div className="text-left">
+              <h3 className="text-lg font-bold">{action.name}</h3>
+              <p className="text-gray-400">{action.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
